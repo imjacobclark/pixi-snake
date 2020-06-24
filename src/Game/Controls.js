9951d453b70668982beef59c12068a630f40a423
Jacob Clark <jacob.jh.clark@googlemail.com>
2020-06-24 11:04:13 +0100

    Refactor

--- src/Game/Controls.js
+++ src/Game/Controls.js
@@ -9,77 +9,73 @@ export default class Controls {
   }
 
   create() {
-    return new Promise((resolve, reject) => {
-      const pauseOrResumeGame = () => {
-        if (this.gameStatus.status === RUNNING) {
-          this.gameStatus.setPaused();
-        } else {
-          this.gameStatus.setRunning();
-        }
+    const pauseOrResumeGame = () => {
+      if (this.gameStatus.status === RUNNING) {
+        this.gameStatus.setPaused();
+      } else {
+        this.gameStatus.setRunning();
+      }
+    }
+
+    document.addEventListener('keydown', (e) => {
+      if (e.keyCode === 38 || e.keyCode === 87 || e.keyCode === 90) {
+        this.snake.up();
       }
 
-      document.addEventListener('keydown', (e) => {
-        if (e.keyCode === 38 || e.keyCode === 87 || e.keyCode === 90) {
-          this.snake.up();
-        }
-  
-        if (e.keyCode === 39 || e.keyCode === 68) {
-          this.snake.right();
-        }
-  
-        if (e.keyCode === 40 || e.keyCode === 83) {
-          this.snake.down();
-        }
-  
-        if (e.keyCode === 37 || e.keyCode === 65 || e.keyCode === 81) {
-          this.snake.left();
-        }
-  
-        if (e.keyCode === 32) {
-          pauseOrResumeGame();
-        }
-      });
-  
-      const handleVisibilityChange = () => {
+      if (e.keyCode === 39 || e.keyCode === 68) {
+        this.snake.right();
+      }
+
+      if (e.keyCode === 40 || e.keyCode === 83) {
+        this.snake.down();
+      }
+
+      if (e.keyCode === 37 || e.keyCode === 65 || e.keyCode === 81) {
+        this.snake.left();
+      }
+
+      if (e.keyCode === 32) {
         pauseOrResumeGame();
       }
-  
-      document.addEventListener('webkitvisibilitychange', handleVisibilityChange);
-      document.addEventListener('visibilitychange', handleVisibilityChange);
-  
-      const recognizer = speechCommands.create("BROWSER_FFT");
-      recognizer.ensureModelLoaded().then(() => {
-        const words = recognizer.wordLabels();
-  
-        recognizer.listen(result => {
-          for (let i = 0; i < words.length; ++i) {
-            if(result.scores[i] > 0.8) {
-              console.log(words[i])
-              switch(words[i]){
-                case 'up':
-                  this.snake.up();
-                  break;
-                case 'right':
-                  this.snake.right();
-                  break;
-                case 'down':
-                  this.snake.down();
-                  break;
-                case 'left':
-                  this.snake.left();
-                  break;
-              }
+    });
+
+    const handleVisibilityChange = () => {
+      pauseOrResumeGame();
+    }
+
+    document.addEventListener('webkitvisibilitychange', handleVisibilityChange);
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    const recognizer = speechCommands.create("BROWSER_FFT");
+    return recognizer.ensureModelLoaded().then(() => {
+      const classLabels = recognizer.wordLabels();
+
+      return recognizer.listen(result => {
+        const scores = result.scores;
+        const max = Math.max(...scores);
+        for (let i = 0; i < classLabels.length; i++) {
+          if (max.toFixed(2) === result.scores[i].toFixed(2)) {
+            switch(classLabels[i]){
+              case 'up':
+                this.snake.up();
+                break;
+              case 'right':
+                this.snake.right();
+                break;
+              case 'down':
+                this.snake.down();
+                break;
+              case 'left':
+                this.snake.left();
+                break;
             }
           }
-        }, 
-        {
-          includeSpectrogram: true,
-          probabilityThreshold: 0.75
-        });
-
-        resolve();
+        }
+      }, 
+      {
+        includeSpectrogram: true,
+        probabilityThreshold: 0.75
       });
-
     });
   }
 }
